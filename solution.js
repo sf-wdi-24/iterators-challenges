2fee40247afe600aaafa10495d6f872f6730c7b9
tkhuynh <thequest2311@yahoo.com>
2015-11-02 17:49:18 -0800

    done, last function similar to Steve as discussed together, will find another way

---
+++ solution.js
@@ -0,0 +1,63 @@
+function each (array, callback) {
+	for (var i = 0; i < array.length; i++) {
+		callback(array[i]);
+	}
+}
+
+function map (array, callback) {
+	var newArray = [];
+	for (var i = 0 ; i < array.length; i++) {
+		newArray.push(callback(array[i]));
+	}
+	return newArray;
+}
+
+function reduce(array) {
+	var sum = 0;
+	for (var i = 0; i < array.length; i++) {
+		sum += array[i];
+	}
+	return sum;
+}
+
+function filter(arr, truthTest) {
+	var newArray = [];
+	for (var i = 0; i < arr.length; i++) {
+		if (truthTest(arr[i])) {
+			newArray.push(arr[i]);
+		}
+	}
+	return newArray;
+}
+
+function partition(arr, truthTest) {
+	var pass = [];
+	var fail = [];
+	for (var i = 0; i < arr.length; i++) {
+		if (truthTest(arr[i])) {
+			pass.push(arr[i]);
+		} else {
+			fail.push(arr[i]);
+		}
+	}
+	return pass.concat(fail);
+}
+
+function pluck(arr, key) {
+	var result = [];
+	for (var i = 0; i < arr.length; i++) {
+		result.push(arr[i][key]);
+	}
+	return result;
+}
+
+function where(arr, keys) {
+    for (var x in keys) {
+        for (var i = arr.length - 1; i >=0; i--) {
+            if (arr[i][x] !== keys[x]) {
+                arr.splice(i, 1);
+            }
+        }
+    }
+    return arr;
+}
